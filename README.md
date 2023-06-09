71e5164bb247706ca67ab8f7bc754d538015c238
gyokuro06 <gyokuro06@gmail.com>
2023-06-09 23:50:46 +0900

    create project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pick-mock
+
+<!-- <img src='https://github.com/gyokuro06/REPO/blog/main/PATH/IMAGE_FILE?raw=true' width='300'> -->
+
+## Overview
+
+## Usage
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import DeliveryListPage from './pages/DeliveryListPage.vue';
+</script>
+
+<template>
+  <DeliveryListPage></DeliveryListPage>
+</template>
+
+<style scoped>
+
+</style>
